f41650a5d8ab9397479b23c10bbb8b2895daa2c9
Gjorgji-Noveski <g.noveski@yahoo.com>
2020-10-30 00:27:37 +0100

    Added Up/Down movement to platforms, collision detection and ability to restart

--- PlatformBuilder.py
+++ PlatformBuilder.py
@@ -7,9 +7,12 @@ class Platform:
     _GREEN = (0, 255, 0)
     _COLORS = (_RED, _GREEN)
 
-    def __init__(self, surface, yPos):
+    def __init__(self, surface, yPos, xPos=None):
         color = random.choice(self._COLORS)
-        width = 200
-        platform = draw.rect(surface, color, (800, yPos, width, 50))
+        width = 250
+        if xPos is not None:
+            platform = draw.rect(surface, color, (xPos, yPos, width, 25))
+        else:
+            platform = draw.rect(surface, color, (800, yPos, width, 25))
         self.color = color
         self.rect = platform
--- main.py
+++ main.py
@@ -5,28 +5,34 @@ import pygame as pyg
 from PlatformBuilder import Platform
 
 pyg.init()
+# Surfaces
 screen = pyg.display.set_mode((800, 600))
 platform_spawn_area = pyg.Surface((1600, 600))
 player = pyg.draw.rect(platform_spawn_area, (0, 0, 250), (50, const.WINDOW_HEIGHT // 2 - 25, 50, 50))
+
+# Variables
 platforms = deque([], 10)
 SPAWN_NEW_PLATFORMS = pyg.USEREVENT
-pyg.time.set_timer(SPAWN_NEW_PLATFORMS, 2000)
+pyg.time.set_timer(SPAWN_NEW_PLATFORMS, 1500)
 clock = pyg.time.Clock()
 goingUP = False
 goingDOWN = False
+movingPlatforms = None
+movingPlatformSpeed = 1
+gameActive = True
 
 
 def createPlatforms():
     if len(platforms) == 0:
         platformBottom = Platform(platform_spawn_area, player.bottomleft[1] + player.height)
         platformTop = Platform(platform_spawn_area, player.topleft[1] - player.height * 2)
-        platforms.append(platformBottom)
         platforms.append(platformTop)
+        platforms.append(platformBottom)
     if platforms[-1].rect.right < 800:
         platformBottom = Platform(platform_spawn_area, player.bottomleft[1] + player.height)
         platformTop = Platform(platform_spawn_area, player.topleft[1] - player.height * 2)
-        platforms.append(platformBottom)
         platforms.append(platformTop)
+        platforms.append(platformBottom)
 
 
 def movePlatforms():
@@ -39,23 +45,58 @@ def drawPlatforms():
         pyg.draw.rect(platform_spawn_area, platform.color, platform.rect)
 
 
-def movingUp():
+def movePlatform(platforms, direction):
+    global movingPlatformSpeed
+    global movingPlatforms
     global goingUP
-    next_platform = None
-    for platform in platforms:#OVDE GI IMA I GORNATA I DOLNATA PLATFORMA
-        if platform.rect.y < player.y:
-            next_platform = platform
-            break
-    if next_platform is not None:
-        while next_platform.rect.y < player.y:
-            next_platform.rect.move_ip(0,5)
+    global goingDOWN
+    if direction == 'DOWN':
+        movingPlatformSpeed -= 1
+    else:
+        movingPlatformSpeed += 1
+    for platform in platforms:
+
+        if direction == 'UP':
+            if platforms[0].rect.y < player.bottom:
+                platform.rect.move_ip(0, movingPlatformSpeed)
+            else:
+                platforms[0].rect.y = player.bottom
+                movingPlatformSpeed = 1
+                movingPlatforms = None
+                goingUP = False
+                goingDOWN = False
+        else:
+            if platforms[1].rect.y > player.bottom:
+                platform.rect.move_ip(0, movingPlatformSpeed)
+            else:
+                platforms[1].rect.y = player.bottom
+                movingPlatformSpeed = 1
+                movingPlatforms = None
+                goingUP = False
+                goingDOWN = False
+
+
+# Looking for the bottom platform
+def findClosestPlatform(direction):
+    global goingUP
+    global movingPlatforms
+    if movingPlatforms is not None:
+        movePlatform(movingPlatforms, direction)
+    else:
+        for idx, platform in enumerate(platforms):  # OVDE GI IMA I GORNATA I DOLNATA PLATFORMA
+            if platform.rect.x > player.right:
+                movingPlatforms = [platform, platforms[idx + 1]]
+                break
+
+
+def detectCollision():
+    for platform in platforms:
+        if platform.rect.colliderect(player):
+            return False
+    return True
 
 
 # TODO: PREJAKA IDEJA, namesto da odish na platformata, vlezi vo nea, kako vo tunel, zelen crven
-def movingDown():
-    pass
-
-
 while True:
     for event in pyg.event.get():
         if event.type == pyg.QUIT:
@@ -64,21 +105,28 @@ while True:
         if event.type == SPAWN_NEW_PLATFORMS:
             createPlatforms()
         if event.type == pyg.KEYDOWN:
+            # Remember, we are moving the platforms not the player, goingUP will bring the platforms down,
+            # and vice versa
             if event.key == pyg.K_UP:
                 goingUP = True
             if event.key == pyg.K_DOWN:
                 goingDOWN = True
-    platform_spawn_area.fill((0, 0, 0))
+            if event.key == pyg.K_r and gameActive == False:
+                platforms.clear()
+                gameActive = True
 
-    movePlatforms()
-    if goingUP:
-        movingUp()
-    if goingDOWN:
-        movingDown()
-    drawPlatforms()
-    player = pyg.draw.rect(platform_spawn_area, (0, 0, 250), player)
-    screen.blit(platform_spawn_area, (0, 0))
+    if gameActive:
+        platform_spawn_area.fill((0, 0, 0))
+        movePlatforms()
+        if goingUP:
+            findClosestPlatform(direction='UP')
+        if goingDOWN:
+            findClosestPlatform(direction='DOWN')
+        gameActive = detectCollision()
+        drawPlatforms()
+        player = pyg.draw.rect(platform_spawn_area, (0, 0, 250), player)
+        screen.blit(platform_spawn_area, (0, 0))
 
-    pyg.display.update()
-    clock.tick(60)
+        pyg.display.update()
+        clock.tick(60)
 pyg.quit()
