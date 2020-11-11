6e5da083c758d2cb661d430ea52e9da919ad76d2
Gjorgji-Noveski <g.noveski@yahoo.com>
2020-11-11 02:36:12 +0100

    Changed color of platform to be set manualy instead of random

--- PlatformBuilder.py
+++ PlatformBuilder.py
@@ -3,12 +3,9 @@ from pygame import draw
 
 
 class Platform:
-    _RED = (255, 0, 0)
-    _GREEN = (0, 255, 0)
-    _COLORS = (_RED, _GREEN)
 
-    def __init__(self, surface, yPos, xPos=None):
-        color = random.choice(self._COLORS)
+    def __init__(self, surface,color, yPos, xPos=None):
+
         width = 250
         if xPos is not None:
             platform = draw.rect(surface, color, (xPos, yPos, width, 25))
