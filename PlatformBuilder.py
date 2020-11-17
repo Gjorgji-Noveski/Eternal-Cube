d4973c92489bd3ffab295216b44d29e518c2f5c1
Gjorgji-Noveski <g.noveski@yahoo.com>
2020-11-17 02:50:27 +0100

    Changed constructor, allowing only to set yPos

--- PlatformBuilder.py
+++ PlatformBuilder.py
@@ -1,15 +1,11 @@
-import random
 from pygame import draw
 
 
 class Platform:
 
-    def __init__(self, surface,color, yPos, xPos=None):
+    def __init__(self, surface,color, yPos):
 
         width = 250
-        if xPos is not None:
-            platform = draw.rect(surface, color, (xPos, yPos, width, 25))
-        else:
-            platform = draw.rect(surface, color, (800, yPos, width, 25))
+        platform = draw.rect(surface, color, (800, yPos, width, 25))
         self.color = color
         self.rect = platform
