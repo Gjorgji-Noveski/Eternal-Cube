8a87e8218739c344ab24637a100f11a5b96d0521
Gjorgji-Noveski <g.noveski@yahoo.com>
2020-11-18 20:04:43 +0100

    Added a score and highscore function

--- main.py
+++ main.py
@@ -6,31 +6,45 @@ from PlatformBuilder import Platform
 import random
 
 pyg.init()
+
 # Surfaces
 screen = pyg.display.set_mode((800, 600))
-platform_spawn_area = pyg.Surface((1600, 600))
+platform_spawn_area = pyg.Surface((1600, 600)).convert()
 player = pyg.draw.rect(platform_spawn_area, (0, 0, 250), (50, const.WINDOW_HEIGHT // 2 - 25, 50, 50))
+
+# Events ( USER event IDs should be between 24 and 32 )
+SPAWN_NEW_PLATFORMS = 24
+UPDATE_SCORE = 25
 
 # Variables
 platforms = deque([], 10)
-SPAWN_NEW_PLATFORMS = pyg.USEREVENT
 pyg.time.set_timer(SPAWN_NEW_PLATFORMS, 1500)
+pyg.time.set_timer(UPDATE_SCORE, 1000)
 clock = pyg.time.Clock()
 goingUP = False
 goingDOWN = False
 closestPlatforms = None
 platformSpeed = 1
 gameActive = False
-font = pyg.font.Font(pyg.font.get_default_font(), 80)
+homeScreenFont = pyg.font.Font(pyg.font.get_default_font(), 80)
+scoreFont = pyg.font.Font(pyg.font.get_default_font(), 35)
+highScore = 0
+score = 0
+
+
+def updateScore():
+    global score
+    score += 1
 
 
 def reinitializeVariables():
-    global platforms, goingUP, goingDOWN, closestPlatforms, platformSpeed, gameActive
+    global platforms, goingUP, goingDOWN, closestPlatforms, platformSpeed, gameActive, score
     platforms.clear()
     goingUP = False
     goingDOWN = False
     closestPlatforms = None
     platformSpeed = 1
+    score = 0
 
 
 def createPlatforms():
@@ -51,7 +65,7 @@ def drawPlatforms():
         pyg.draw.rect(platform_spawn_area, platform.color, platform.rect)
 
 
-def movePlatform(platforms, direction):
+def moveClosestPlatforms(platforms, direction):
     global platformSpeed
     global closestPlatforms
     global goingUP
@@ -81,6 +95,7 @@ def movePlatform(platforms, direction):
                 goingUP = False
                 goingDOWN = False
 
+
 def checkIfPlayerMoved():
     global closestPlatforms
     if closestPlatforms is not None and closestPlatforms[0].rect.left < player.right:
@@ -88,6 +103,7 @@ def checkIfPlayerMoved():
             return True
         return False
     return True
+
 
 # Looking for platforms that are the closest to the cube
 def findClosestPlatform():
@@ -98,23 +114,40 @@ def findClosestPlatform():
             break
 
 
+def renderScore():
+    global scoreFont, score, platform_spawn_area
+    scoreSurf = scoreFont.render("Score: %s" % score, True, const.WHITE, const.BLACK).convert()
+    scoreRect = scoreSurf.get_rect()
+    scoreRect.topright = (const.WINDOW_WIDTH, 0)
+    platform_spawn_area.blit(scoreSurf, scoreRect)
+
 
 def homeScreen(mousePos, pressedButtons):
-    global screen, font, gameActive
-    gameTitleSurf = font.render("Eternal Cube", True, (150, 150, 150), const.BLACK)
+    global screen, homeScreenFont, gameActive, scoreFont, highScore
+    gameTitleSurf = homeScreenFont.render("Eternal Cube", True, (150, 150, 150), const.BLACK).convert()
     gameTitleRect = gameTitleSurf.get_rect()
     gameTitleRect.midbottom = (const.WINDOW_WIDTH / 2, const.WINDOW_HEIGHT / 3)
-    startSurf = font.render("START", True, const.BLUE, const.BLACK)
+    startSurf = homeScreenFont.render("START", True, const.BLUE, const.BLACK).convert()
     startSurfRect = startSurf.get_rect()
     startSurfRect.center = (const.WINDOW_WIDTH / 2, const.WINDOW_HEIGHT - const.WINDOW_HEIGHT / 3)
     # creating hover animation on Start button
     if startSurfRect.collidepoint(mousePos) and not gameActive:
-        startSurf = font.render("START", True, const.BLUE, (150, 150, 150))
+        startSurf = homeScreenFont.render("START", True, const.BLUE, (150, 150, 150)).convert()
         # starting the game when left clicking
         if pressedButtons[0]:
             reinitializeVariables()
             gameActive = True
-    screen.blits(((gameTitleSurf, gameTitleRect, None), (startSurf, startSurfRect, None)))
+    updateHighScore()
+    highScoreSurf = scoreFont.render("High Score %s" % highScore, True, const.WHITE, const.BLACK).convert()
+    highScoreRect = highScoreSurf.get_rect()
+
+    screen.blits(((gameTitleSurf, gameTitleRect, None), (startSurf, startSurfRect, None),(highScoreSurf, highScoreRect, None)))
+
+
+def updateHighScore():
+    global highScore, score
+    if score > highScore:
+        highScore = score
 
 
 def detectCollisionWithRed():
@@ -124,12 +157,11 @@ def detectCollisionWithRed():
             return True
     return False
 
-while True:
 
+while True:
     # TODO: PREJAKA IDEJA, namesto da odish na platformata, vlezi vo nea, kako vo tunel, zelen crven
     # TODO: ne se oslonuvaj kreiranje na platformi vremenski na odredeni intervali, deka ako korisnikot ima mal fps
     #  ke bidat nabutani tuku kreiraj koga vishe ednata platforma ke bide vo odredena lokacija na ekranot
-
     for event in pyg.event.get():
         if event.type == pyg.QUIT:
             pyg.quit()
@@ -143,6 +175,8 @@ while True:
                 goingUP = True
             if event.key == pyg.K_DOWN:
                 goingDOWN = True
+        if event.type == UPDATE_SCORE and gameActive:
+            updateScore()
 
     homeScreen(pyg.mouse.get_pos(), pyg.mouse.get_pressed())
     if gameActive:
@@ -150,16 +184,18 @@ while True:
         movePlatforms()
         findClosestPlatform()
         if goingUP and closestPlatforms:
-            movePlatform(closestPlatforms, direction='UP')
+            moveClosestPlatforms(closestPlatforms, direction='UP')
         if goingDOWN and closestPlatforms:
-            movePlatform(closestPlatforms, direction='DOWN')
+            moveClosestPlatforms(closestPlatforms, direction='DOWN')
         if goingUP is not True and goingDOWN is not True:
             gameActive = checkIfPlayerMoved()
             if detectCollisionWithRed() is True:
                 gameActive = False
         drawPlatforms()
         player = pyg.draw.rect(platform_spawn_area, (0, 0, 250), player)
+        renderScore()
         screen.blit(platform_spawn_area, (0, 0))
+
 
     pyg.display.update()
     clock.tick(60)
