f012a36adeb019cf0d11f6b9504580e70ebf9eed
Gjorgji-Noveski <g.noveski@yahoo.com>
2020-11-18 02:03:32 +0100

    Changed moving logic and added death when the player doesn't go up or down

--- main.py
+++ main.py
@@ -18,10 +18,19 @@ pyg.time.set_timer(SPAWN_NEW_PLATFORMS, 1500)
 clock = pyg.time.Clock()
 goingUP = False
 goingDOWN = False
-movingPlatforms = None
-movingPlatformSpeed = 1
+closestPlatforms = None
+platformSpeed = 1
 gameActive = False
 font = pyg.font.Font(pyg.font.get_default_font(), 80)
+
+
+def reinitializeVariables():
+    global platforms, goingUP, goingDOWN, closestPlatforms, platformSpeed, gameActive
+    platforms.clear()
+    goingUP = False
+    goingDOWN = False
+    closestPlatforms = None
+    platformSpeed = 1
 
 
 def createPlatforms():
@@ -43,47 +52,51 @@ def drawPlatforms():
 
 
 def movePlatform(platforms, direction):
-    global movingPlatformSpeed
-    global movingPlatforms
+    global platformSpeed
+    global closestPlatforms
     global goingUP
     global goingDOWN
     if direction == 'DOWN':
-        movingPlatformSpeed -= 1
+        platformSpeed -= 1
     else:
-        movingPlatformSpeed += 1
+        platformSpeed += 1
     for platform in platforms:
 
         if direction == 'UP':
             if platforms[0].rect.bottom < player.top:
-                platform.rect.move_ip(0, movingPlatformSpeed)
+                platform.rect.move_ip(0, platformSpeed)
             else:
                 platforms[0].rect.bottom = player.top + 1
-                movingPlatformSpeed = 1
-                movingPlatforms = None
+                platformSpeed = 1
+                closestPlatforms = None
                 goingUP = False
                 goingDOWN = False
         else:
             if platforms[1].rect.top > player.bottom:
-                platform.rect.move_ip(0, movingPlatformSpeed)
+                platform.rect.move_ip(0, platformSpeed)
             else:
                 platforms[1].rect.top = player.bottom - 1
-                movingPlatformSpeed = 1
-                movingPlatforms = None
+                platformSpeed = 1
+                closestPlatforms = None
                 goingUP = False
                 goingDOWN = False
 
+def checkIfPlayerMoved():
+    global closestPlatforms
+    if closestPlatforms is not None and closestPlatforms[0].rect.left < player.right:
+        if closestPlatforms[0].rect.colliderect(player) or closestPlatforms[1].rect.colliderect(player):
+            return True
+        return False
+    return True
 
-# Looking for the bottom platform
-def findClosestPlatform(direction):
-    global goingUP
-    global movingPlatforms
-    if movingPlatforms is not None:
-        movePlatform(movingPlatforms, direction)
-    else:
-        for idx, platform in enumerate(platforms):  # OVDE GI IMA I GORNATA I DOLNATA PLATFORMA
-            if platform.rect.x > player.right:
-                movingPlatforms = [platform, platforms[idx + 1]]
-                break
+# Looking for platforms that are the closest to the cube
+def findClosestPlatform():
+    global closestPlatforms, platforms
+    for idx, platform in enumerate(platforms):  # OVDE GI IMA I GORNATA I DOLNATA PLATFORMA
+        if platform.rect.right > player.right:
+            closestPlatforms = [platform, platforms[idx + 1]]
+            break
+
 
 
 def homeScreen(mousePos, pressedButtons):
@@ -95,20 +108,21 @@ def homeScreen(mousePos, pressedButtons):
     startSurfRect = startSurf.get_rect()
     startSurfRect.center = (const.WINDOW_WIDTH / 2, const.WINDOW_HEIGHT - const.WINDOW_HEIGHT / 3)
     # creating hover animation on Start button
-    if startSurfRect.collidepoint(mousePos):
+    if startSurfRect.collidepoint(mousePos) and not gameActive:
         startSurf = font.render("START", True, const.BLUE, (150, 150, 150))
         # starting the game when left clicking
         if pressedButtons[0]:
+            reinitializeVariables()
             gameActive = True
     screen.blits(((gameTitleSurf, gameTitleRect, None), (startSurf, startSurfRect, None)))
 
 
-def detectCollision():
+def detectCollisionWithRed():
+    global platforms
     for platform in platforms:
         if platform.rect.colliderect(player) and platform.color == const.RED:
-            return False
-    return True
-
+            return True
+    return False
 
 while True:
 
@@ -129,19 +143,20 @@ while True:
                 goingUP = True
             if event.key == pyg.K_DOWN:
                 goingDOWN = True
-            if event.key == pyg.K_r and gameActive == False:
-                platforms.clear()
-                gameActive = True
 
     homeScreen(pyg.mouse.get_pos(), pyg.mouse.get_pressed())
     if gameActive:
         platform_spawn_area.fill((0, 0, 0))
         movePlatforms()
-        if goingUP:
-            findClosestPlatform(direction='UP')
-        if goingDOWN:
-            findClosestPlatform(direction='DOWN')
-        gameActive = detectCollision()
+        findClosestPlatform()
+        if goingUP and closestPlatforms:
+            movePlatform(closestPlatforms, direction='UP')
+        if goingDOWN and closestPlatforms:
+            movePlatform(closestPlatforms, direction='DOWN')
+        if goingUP is not True and goingDOWN is not True:
+            gameActive = checkIfPlayerMoved()
+            if detectCollisionWithRed() is True:
+                gameActive = False
         drawPlatforms()
         player = pyg.draw.rect(platform_spawn_area, (0, 0, 250), player)
         screen.blit(platform_spawn_area, (0, 0))
